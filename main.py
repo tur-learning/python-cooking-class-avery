641549bfa29f1a55e47835cf2244065370e4f5cf
Avery Cutler <tut70526@temple.edu>
2025-02-04 22:13:25 +0000

    commit2

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ pesto_genovese.show_ingredients()
 pesto_genovese.check_ingredients()
 pesto_genovese.cook_pasta()
 
+print("\n --- Ricotta Gnocchi Recipe ---")
 ricotta_gnocchi.show_ingredients()
 ricotta_gnocchi.check_ingredients()
 ricotta_gnocchi.cook_pasta()
