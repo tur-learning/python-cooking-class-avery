d2ca59a2003bed4545d9b0c626059bf839efdfeb
maximiliaangadeyne <maximiliaan.gadeyne@temple.edu>
2025-02-06 17:42:44 +0000

    Pappardelle al Cinghiale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from carbonara import carbonara
 from pesto_genovese import pesto_genovese
+from cinghiale import cinghiale
 
 # 🍝 Execute Recipe Steps
 print("\n--- Carbonara Recipe ---")
@@ -11,3 +12,8 @@ print("\n--- Pesto Genovese Recipe ---")
 pesto_genovese.show_ingredients()
 pesto_genovese.check_ingredients()
 pesto_genovese.cook_pasta()
+
+print("\n--- Ragu' di Cinghiale Recipe ---")
+cinghiale.show_ingredients()
+cinghiale.check_ingredients()
+cinghiale.cook_pasta()
